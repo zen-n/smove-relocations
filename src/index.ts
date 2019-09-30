ba4ae72772c82ad66fee5dc33cbadfc48b9d924e
Zenon Colaco <zncolaco@gmail.com>
2019-09-30 17:42:14 +1300

    Parameterise input/output files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-import * as fs from 'fs';
-import { Booking, Solution } from './types';
-import { tree } from './tree';
+import * as fs from "fs";
+import { tree } from "./tree";
+import { Booking, Solution } from "./types";
 
-let data: Booking[] =[];
+if (process.argv[2] === "help") {
+    console.info("Enter arguments in the format 'npm start <input file location> <output file location>'");
+    console.info("If no args are provided defaults will be used");
+    process.exit(0);
+}
 
-try{
-     data = JSON.parse(fs.readFileSync('data/bookings.json', 'utf8'));
+const defaultInputLocation = "data/bookings.json";
+const defaultOutputLocation = "results.json";
+const inputFileLocation = process.argv[2];
+const outputFileLocation = process.argv[3];
+
+if (inputFileLocation) {
+    console.warn(`No input file location provided, defaulting to ${defaultInputLocation}`);
+}
+if (outputFileLocation) {
+    console.warn(`No output file location provided, defaulting to ${defaultOutputLocation}`);
+}
+
+let data: Booking[] = [];
+
+try {
+    data = JSON.parse(fs.readFileSync(inputFileLocation ||  defaultInputLocation, "utf8"));
 } catch (error) {
     console.error(error.message);
     process.exit(1);
@@ -17,7 +35,7 @@ console.debug(`Min relocations: ${solution.relocations}`);
 console.debug(`Optimal bookings: ${JSON.stringify(solution.bookingSet)}`);
 writeToFile(solution.bookingSet);
 
-
-function writeToFile(bookings: Booking[]){
-    fs.writeFileSync('results.json', JSON.stringify(bookings.map((booking) => booking.id)));
+function writeToFile(bookings: Booking[]) {
+    fs.writeFileSync(outputFileLocation || defaultOutputLocation,
+        JSON.stringify(bookings.map((booking) => booking.id)));
 }
